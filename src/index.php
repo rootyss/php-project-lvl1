aa30e1b444262d093e3e4e7d3426e2807478b0fa
v.krivshuk <krivshuk@ispras.ru>
2022-10-23 20:15:37 +0300

    fix index

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ namespace BrainGames\Index;
 use function cli\line;
 use function cli\prompt;
 
-function meetingUser()
+function meetingUser(): string
 {
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
