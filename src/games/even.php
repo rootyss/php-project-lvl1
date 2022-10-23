168cf2477e883f0e9d38d52f0b5bbff7fb92f57c
v.krivshuk <krivshuk@ispras.ru>
2022-10-23 18:28:13 +0300

    added even-game

---
+++ src/games/even.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function BrainGames\Index\startGame;
+
+function isEven($number)
+{
+        return $number % 2 === 0;
+}
+
+function buildEvenGame()
+{
+    $genGameQuestion = function () {
+        $question = rand(1, 100);
+        $rightAnswer = isEven($question) ? "yes" : "no";
+
+        return [$question, $rightAnswer];
+    };
+    $description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+    startGame($description, $genGameQuestion);
+}
---
+++ src/index.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BrainGames\Index;
+
+use function cli\line;
+use function cli\prompt;
+
+function meetingUser()
+{
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    return $name;
+}
+
+function startGame($description, callable $importGameData)
+{
+    $roundsCount = 3;
+    line('Welcome to the Brain Game!');
+    $userName = meetingUser();
+    line($description);
+
+    $rightAnswerCount = 0;
+
+    while ($rightAnswerCount < $roundsCount) {
+        $gameData = $importGameData();
+        $question = $gameData[0];
+        $rightAnswer = $gameData[1];
+        line("Question: %s!", $question);
+
+        $answer = prompt('Your answer: ');
+
+        if ($answer === $rightAnswer) {
+            line("Correct!");
+            $rightAnswerCount += 1;
+        } else {
+            line("%s is wrong answer ;(. Correct answer was %s", $answer, $rightAnswer);
+            line("Let's try again, %s", $userName);
+            return;
+        }
+    }
+    line("Congratulations, %s", $userName);
+}
